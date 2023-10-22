4476697fc710f53140a44ae0b041750f1e950f7a
chrismdks <chris.mdks@gmail.com>
2023-10-22 17:18:42 +0300

    added endpoint to get all menu items & created one format for the api response

---
+++ RedMango_API/Models/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace RedMango_API.Models
+{
+    public class ApiResponse
+    {
+        public ApiResponse() { ErrorMessages = new List<string>(); } // constructor
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
